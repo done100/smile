453eafe6745552a5a17793a0b8fd73b8634fcbe6
done100 <78077290+done100@users.noreply.github.com>
2021-08-10 17:47:59 +0800

    Delete smile directory

--- smile/work/src/mian/java/com/stars/sea/work/dao/TgVideoCategoryMapper.java
+++
@@ -1,9 +0,0 @@
-package com.stars.sea.work.dao;
-
-import com.stars.sea.work.bean.OpenkTgVideoCategoryDTO;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface TgVideoCategoryMapper {
-    OpenkTgVideoCategoryDTO selectVideoCodeByIds(String id);
-}
--- smile/work/src/mian/java/com/stars/sea/work/service/AiService.java
+++
@@ -1,8 +0,0 @@
-package com.stars.sea.work.service;
-
-import org.springframework.stereotype.Service;
-
-
-public interface AiService {
-    Integer getDocterCount();
-}
